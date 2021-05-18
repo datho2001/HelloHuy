9015dee4aff3b84c98426871310352eb39262d47
datho2001 <83498189+datho2001@users.noreply.github.com>
2021-05-18 21:15:52 +0700

    Add files via upload

---
+++ IdeaStudent/wwwroot/js/js.js
@@ -0,0 +1,15 @@
+ $(document).ready(function () {
+     // $container = $('aside'),
+     // $scrollTo = $('footer'),
+     // $container.animate({
+     //     scrollTop: $scrollTo.offset().top - $container.offset().top + $container.scrollTop
+     // });
+     $('input').click(function () {
+         alert("hello");
+         $('#aside').hidden();
+     })
+     $('button').click(function () {
+         alert("hello cc");
+         $('#aside').show();
+     })
+ });
